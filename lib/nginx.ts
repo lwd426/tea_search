646e81a501252ccaed4745dd51c7e65e15c4f00c
zhuoyongran <zhuoyongran@le.com>
2017-04-10 17:18:18 +0800

    ...

--- lib/nginx.ts
+++ lib/nginx.ts
@@ -297,25 +297,5 @@ function nginx(arr: any[]) {
     return re;
 }
 
-/*nginx([
-        {
-            url:'/',
-            regionArray: ['bj', 'shanghai'],
-            serverArray: ["1.1.1.1", "2.2.2.8"],
-            uidArray: [],
-            default: false
-        },
-        {
-            url: ['/b.html', '/c.html'],
-            uidArray: ['bj', 'shanghai'],
-            serverArray: ["1.1.1.2", "2.2.2.2"],
-            default: false
-        },
-        {
-            url: ['/'],
-            serverArray: ['3.3.3.3', '4.4.4.4', '5.5.5.5', '6.6.6.6'],
-            default: 1
-        }
-    ]
-);*/
+
 export = nginx;
