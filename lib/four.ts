82171b80248c83bec57f319b9b35a084d2437ca6
zhuoyongran <zhuoyongran@le.com>
2017-04-21 11:27:47 +0800

    ...

--- lib/four.ts
+++ lib/four.ts
@@ -31,7 +31,6 @@ export class four {
             s += `
             location ${v} {
                 ${u}
-                #default
                 proxy_pass http://${n}_default;
             }  
             `;
