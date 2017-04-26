fc24b39595655f9caecc98a0bfd730997398dce9
houzezhou <houzezhou77@163.com>
2017-04-26 18:53:05 +0800

    fix bug

--- frontend/components/mainpage/Traffic.js
+++ frontend/components/mainpage/Traffic.js
@@ -49,7 +49,8 @@ export default class EChart extends React.Component {
         let startTime = moment(new Date(date_picker[0])).format('YYYY-MM-DD');
         let endTime = moment(new Date(date_picker[1])).format('YYYY-MM-DD');
         let res = request.getTrafficDataByStragety(stragety_arr, startTime, endTime, (res)=>{
-            if(res.result.status == 'error'){
+            console.log(res)
+            if(!res || !res.result || res.result.status == 'error'){
                 return
             }
             let responseData = res.result.data;
@@ -269,9 +270,9 @@ export default class EChart extends React.Component {
                     ],
                 });
 
-                _this.setState({
-                    tableData: [],
-                })
+                // _this.setState({
+                //     tableData: [],
+                // })
             }
         });
 
